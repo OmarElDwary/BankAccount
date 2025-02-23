1347aa73c35eb4a4a311cc4b2a65a818bd8461c8
Omar Shaban ElDwary <105021882+OmarElDwary@users.noreply.github.com>
2025-02-23 15:10:33 +0200

    Update account.dart

--- account.dart
+++ account.dart
@@ -2,7 +2,6 @@ abstract class Account {
   int _accountId;
   double _accountBalance;
 
-  // create constructors and initialize the balance to be 0 by default;
   Account(this._accountId, [this._accountBalance = 0]);
 
   Account.startingBalance(this._accountId) : _accountBalance = 0;
@@ -23,7 +22,6 @@ abstract class Account {
     if (money <= 0) {
       print('Amount must be greater than 0');
     } else if (_accountBalance - money >= 0) {
-      // ensure that the user have available balance;
       _accountBalance -= money;
       print('successfull withdraw of $money');
     } else if (_accountBalance - money < 0) {
@@ -52,7 +50,6 @@ class Admin {
   int adminId;
   Admin(this.adminName, this.adminId);
 
-  // create a method to deduct the adminestrative fees from the user;
   void deductAdminFees(UserAccount user, double fees) {
     user._accountBalance -= fees;
   }
